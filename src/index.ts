00e7d9258a70aeacfa26f0aef1430bcf53a081f0
Yi Hao <yi.a.hao@accenture.com>
2020-09-22 13:52:36 -0500

    Small Adjustment regarding Artboard naming rule. Modify Package Information.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { AssistantPackage, RuleContext, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
+const emojiStrip = require('emoji-strip')
 
 const artboardNameCheck: RuleDefinition = {
   rule: async (context) => {
     const { utils } = context;
     for (const page of utils.objects.page) {
+      const pageName = emojiStrip(page.name).trim();
       for (const artboard of page.layers) {
         if (artboard._class !== 'artboard') { continue; };
-        if (!artboard.name.startsWith(page.name + " - "))
-          context.utils.report(`Artboard "${artboard.name}"'s Name should start with "${page.name} - "`);
+        if (artboard.name == pageName) { continue; };
+        if (!artboard.name.startsWith(pageName + " - "))
+          context.utils.report(`Artboard "${artboard.name}"'s Name should start with "${pageName} - "`);
       }
     }
   },
@@ -22,6 +25,7 @@ const pageNameCheck: RuleDefinition = {
     const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
     for (const page of utils.objects.page) {
       if (page.name === "Symbols") { continue; };
+
       const firstCharacter = [...page.name][0];
       const paragraph = "" + firstCharacter;
       if (!paragraph.match(regex)) {
@@ -48,7 +52,7 @@ const groupNameCheck: RuleDefinition = {
         reportGroupNameViolation(context, group.name);
         continue;
       }
-      if (nameComponents[nameComponents.length - 1] === "Copy") {
+      if (nameComponents.length > 1 && nameComponents[nameComponents.length - 1] === "Copy") {
         reportGroupNameViolation(context, group.name);
       }
     }
