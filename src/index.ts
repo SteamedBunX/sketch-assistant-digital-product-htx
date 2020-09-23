29eb9bb9328ae473b7d2b08b3f1507906aa53da0
Yi Hao <yi.a.hao@accenture.com>
2020-09-23 09:26:19 -0500

    Fix bug with Ignored object Add Hierachy check for Archived page/artboard/group

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ const artboardNameCheck: RuleDefinition = {
   rule: async (context) => {
     const { utils } = context;
     for (const page of utils.objects.page) {
+      if (page.name === "Symbols" || isArchived(page.name)) { continue; };
       const pageName = emojiStrip(page.name).trim();
       for (const artboard of page.layers) {
         if (artboard._class !== 'artboard'
           || artboard.name == pageName
-          || isArchived(artboard.name)) { continue; };
+          || isArchived(artboard.name)
+          || utils.isObjectIgnored(artboard)) { continue; };
 
         if (!artboard.name.startsWith(pageName + " - "))
           context.utils.report(`Artboard "${artboard.name}"'s Name should start with "${pageName} - "`, artboard);
@@ -26,13 +28,13 @@ const pageNameCheck: RuleDefinition = {
     const { utils } = context;
     const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
     for (const page of utils.objects.page) {
-      if (page.name === "Symbols"
-        || isArchived(page.name)) { continue; };
+      // ignore the automatically generated Symbols page which holds all the symbols
+      if (page.name === "Symbols") { continue; };
 
       const firstCharacter = [...page.name][0];
       const paragraph = "" + firstCharacter;
       if (!paragraph.match(regex)) {
-        context.utils.report(`Page "${page.name}" should start with an emoji`, page);
+        utils.report(`Page "${page.name}" should start with an emoji`, page);
       }
     }
   },
@@ -45,19 +47,28 @@ const groupNameCheck: RuleDefinition = {
   rule: async (context) => {
     const { utils } = context;
     const numberRegex = new RegExp('^\\d+$');
-    for (const group of utils.objects.group) {
-      if (isArchived(group.name)) { continue; };
-      if (group.name === "Group") {
-        reportGroupNameViolation(context, group.name, group);
-        continue;
-      }
-      const nameComponents = group.name.split(' ');
-      if (nameComponents.length == 2 && nameComponents[0] === "Group" && nameComponents[1].match(numberRegex)) {
-        reportGroupNameViolation(context, group.name, group);
-        continue;
-      }
-      if (nameComponents.length > 1 && nameComponents[nameComponents.length - 1] === "Copy") {
-        reportGroupNameViolation(context, group.name, group);
+
+    for (const page of utils.objects.page) {
+      if (isArchived(page.name)) { continue; };
+      for (const artboard of page.layers) {
+        if (artboard._class !== 'artboard' || isArchived(artboard.name)) { continue; };
+        for (const group of artboard.layers) {
+          if (isArchived(group.name)
+            || group._class !== 'group'
+            || utils.isObjectIgnored(group)) { continue; };
+          if (group.name === "Group") {
+            reportGroupNameViolation(context, group.name, group);
+            continue;
+          }
+          const nameComponents = group.name.split(' ');
+          if (nameComponents.length == 2 && nameComponents[0] === "Group" && nameComponents[1].match(numberRegex)) {
+            reportGroupNameViolation(context, group.name, group);
+            continue;
+          }
+          if (nameComponents.length > 1 && nameComponents[nameComponents.length - 1] === "Copy") {
+            reportGroupNameViolation(context, group.name, group);
+          }
+        }
       }
     }
   },
@@ -76,7 +87,8 @@ const symbolNameCheck: RuleDefinition = {
     for (const page of utils.objects.page) {
       if (page.name === 'Symbols') {
         for (const symbol of page.layers) {
-          if (isArchived(symbol.name)) { continue; };
+          if (isArchived(symbol.name)
+            || utils.isObjectIgnored(symbol)) { continue; };
           if (symbol.name.split('/').length <= 1) {
             context.utils.report(`Symbol "${symbol.name}" should use forward slash grouping`, symbol);
           }
